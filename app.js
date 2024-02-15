66475ef26ca8bf935d2e015f09ff8a1168c9292e
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-02-15 12:38:58 +0300

    added other files

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    // port: process.env.PORT
+    port: process.env.PORT
 });
 connection.connect((err) => {
     if(err) {
@@ -30,14 +30,96 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 app.get('/library',(req, res) => {
-    const query = "SELECT book_name, book_publisher, COUNT(*) AS book_count FROM books GROUP BY book_name;";
+    const query = "SELECT book_id, book_name, book_publisher, COUNT(*) AS book_count FROM books GROUP BY book_name;";
     connection.query(query, (err, results) => {
         if(err) {
             console.log(err);
             return;
         }
-        res.render('library', {books: results});
+        const query1 = "SELECT member_fname, member_lname FROM members;";
+        connection.query(query1, (err, results1) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            res.render('library', {books: results, members: results1});
+        })
+
     });
+
+
+})
+
+//specific book page
+app.get('/library/book/:id', (req, res) => {
+    const id = req.params.id;
+
+    const query = "SELECT * FROM books WHERE book_id = ?;";
+    const values = [id];
+    connection.query(query, values, (err, result) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        res.render('book', {details: result});
+    })
+
+})
+
+//specific user
+app.get('/library/user/:id', (req, res) => {
+    const id = req.params.id;
+
+    const query = "SELECT * FROM members WHERE member_id = ?;";
+    const values = [id];
+
+    connection.query(query, values, (err, result) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        res.render('user', {details: result});
+    })
+})
+
+//regist users
+app.post('/addMember', (req, res) => {
+    const data = req.body;
+
+    const query0 = "SELECT member_fname, member_lname, member_national_id FROM members WHERE member_national_id = ?;";
+    const values0 = [data.userId];
+    connection.query(query0, values0, (err, result) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        if(result.length === 0) {
+            const query = "INSERT INTO members (member_fname, member_lname, member_email, member_national_id) VALUES (?, ?, ?, ?);";
+            const values = [data.userFname, data.userLname, data.userEmail, data.userId];
+            connection.query(query, values, (err) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                
+            })
+        
+            const query1 = "SELECT member_fname, member_lname, member_id FROM members WHERE member_fname = ? AND member_email = ?;";
+            const values1 = [data.userFname, data.userEmail];
+            connection.query(query1, values1, (err, result) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                res.render('regist', {details: result, userExists: 0});
+            })
+            // connection.end();
+        } else {
+            res.render('regist', {details: result, userExists: 1});
+        }
+    })
+
+
 })
 
 app.post('/addBook', (req, res) => {
--- public/home.js
+++ public/home.js
@@ -1,15 +1,10 @@
-let rightContent = document.querySelector('.home-right');
-console.log(rightContent.innerHTML);
+// let rightContent = document.querySelector('.home-right');
+// console.log(rightContent.innerHTML);
 
-let header = document.querySelector('.home-header');
-    if(header.style.width < "800px") {
-        rightContent.innerHTML = 
-        `
-        <select name="<i class='fa-solid fa-bars'></i>" id="">
-            <option value="">MENU</option>
-            <option value=""><a href="">Library</a></option>
-            <option value=""><a href="">about us</a></option>
-            <option value=""><a href="">contacts</a></option>
-        </select>
-        `
-    }
+// let header = document.querySelector('.home-header');
+//     if(header.style.width < '800px') {
+//         rightContent.innerHTML = 
+//         `
+
+//         `
+//     }
