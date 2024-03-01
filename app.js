4fc6752e76e33b994b439a3880865b3e1dd3fc77
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-03-01 21:56:37 +0300

    another commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mysql = require('mysql');
 require('dotenv').config();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 
